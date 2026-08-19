d46ac0dad7dbe4eddd1618c743418c067caba572
Shyam <s7hyamweb@gmail.com>
2026-08-19 10:55:47 +0530

    refactor: add explicit types for model invocation and add safety checks for tool calls in research agent

--- src/agents/research-agent.ts
+++ src/agents/research-agent.ts
@@ -1,15 +1,18 @@
+import type {
+  ChatCompletionMessageParam,
+  ChatCompletionTool,
+} from 'openai/resources'
 import { ai } from '../client/ai-client.js'
 import { MODEL } from '../constants/index.js'
 import { getWeatherTool } from '../tools/weather.tool-definition.js'
 import { getWeather } from '../tools/weather.tool.js'
 
-const invokeModel = async function ({
-  messages,
-  tools,
-}: {
-  tools?: any
-  messages: any
-}) {
+interface InvokeModelParams {
+  messages: ChatCompletionMessageParam[]
+  tools?: ChatCompletionTool[]
+}
+
+const invokeModel = async function ({ messages, tools }: InvokeModelParams) {
   return ai.chat.completions.create({
     model: MODEL,
     messages,
@@ -18,15 +21,13 @@ const invokeModel = async function ({
 }
 
 async function researchAgent(question: string) {
-  const messages: any = [{ role: 'user', content: question }]
-  let count = 0
+  const messages: ChatCompletionMessageParam[] = [
+    { role: 'user', content: question },
+  ]
+
   while (true) {
     const response = await invokeModel({ messages, tools: [getWeatherTool] })
-    console.log(
-      'Invoke Model Response ',
-      count++,
-      JSON.stringify(response, null, 4)
-    )
+
     if (!response.choices[0]?.message.tool_calls?.length) {
       return { message: response.choices[0]?.message }
     }
@@ -35,18 +36,20 @@ async function researchAgent(question: string) {
     messages.push(message)
 
     message.tool_calls?.forEach((toolCall) => {
-      const args = JSON.parse(toolCall.function.arguments)
-      switch (toolCall.function.name) {
-        case 'getWeather':
-          const weatherData = getWeather(args.city)
-          messages.push({
-            role: 'tool',
-            tool_call_id: toolCall.id,
-            content: JSON.stringify(weatherData),
-          })
-          break
-        default:
-          break
+      if (toolCall.type === 'function') {
+        const args = JSON.parse(toolCall.function.arguments)
+        switch (toolCall.function.name) {
+          case 'getWeather':
+            const weatherData = getWeather(args.city)
+            messages.push({
+              role: 'tool',
+              tool_call_id: toolCall.id,
+              content: JSON.stringify(weatherData),
+            })
+            break
+          default:
+            break
+        }
       }
     })
   }
